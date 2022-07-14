beba9613203969cb552871cff4358d749a1ae53d
ankit <ankit@zasyasolutions.com>
2022-07-14 17:12:05 +0530

    table changes

--- component/Review/ReviewCreatedComponent.jsx
+++ component/Review/ReviewCreatedComponent.jsx
@@ -1,20 +1,33 @@
 import React, { useState, useEffect } from "react";
-
+import { Grid } from "antd";
 import { CalendarOutlined } from "@ant-design/icons";
 import moment from "moment";
 import { Modal, Collapse, Skeleton, Row, Col, Table } from "antd";
 import AnswerViewComponent from "./AnswerViewComponent";
 import Link from "next/link";
+import {
+  CalanderIcon,
+  ShareIcon,
+  StarSmallIcon,
+  UserIcon,
+} from "../../assets/Icon/icons";
+const { useBreakpoint } = Grid;
 
 function ReviewCreatedComponent({ user, reviewData }) {
+  const { xs } = useBreakpoint();
   const { Panel } = Collapse;
   const datePattern = "DD-MM-YYYY";
   const [answerData, setAnswerData] = useState([]);
   const [headersData, setHeadersData] = useState([]);
   const [answerDataModel, setAnswerDataModel] = useState(false);
   const [loading, setLoading] = useState(false);
-
   const [dataSource, setDataSource] = useState([]);
+  const [fixed, setFixed] = useState(false);
+
+  useEffect(() => {
+    if (xs) setFixed(xs);
+    else setFixed(xs);
+  }, [xs]);
 
   const applyFilters = (object) => {
     if (object.length > 0) {
@@ -28,10 +41,11 @@ function ReviewCreatedComponent({ user, reviewData }) {
     } else setDataSource([]);
   };
 
+  console.log(fixed, "fixed");
   let nameTitle = {
     title: "Name",
     dataIndex: "name",
-    fixed: "left",
+    fixed: fixed ? false : true,
 
     sorter: (a, b) => a.name?.localeCompare(b.name),
   };
@@ -90,15 +104,101 @@ function ReviewCreatedComponent({ user, reviewData }) {
     <div>
       <div className="px-3 md:px-8 h-auto mt-5">
         <div className="container mx-auto max-w-full">
-          <div className="flex items-end justify-end my-3">
-            <div className="flex ">
-              <Link href="/review">
-                <button className="primary-bg-btn text-white text-sm py-3 text-center px-4 rounded-md ">
-                  Back
-                </button>
-              </Link>
+          <div className="md:flex items-center justify-between text-base font-medium my-4 ">
+            <div className="md:flex items-center ">
+              <div className="primary-color-blue">Monthly Review</div>
+              <div className="flex ml-2">
+                <div className="bg-red-400 py-2 px-2 rounded-full ">
+                  <ShareIcon />
+                </div>
+              </div>
+              <div className="flex  primary-color-blue md:mx-10">
+                <p className="">Type:</p>
+                <p>Feedback</p>
+              </div>
+              <div className="flex  primary-color-blue">
+                <p className="">Created Date:</p>
+                <p>10/11/2018</p>
+              </div>
+            </div>
+
+            <div>
+              <div className="flex items-end justify-end ">
+                <div className="flex ">
+                  <Link href="/review">
+                    <button className="primary-bg-btn text-white text-sm py-3 text-center px-4 rounded-md ">
+                      Back
+                    </button>
+                  </Link>
+                </div>
+              </div>
             </div>
           </div>
+          <Row justify="space-between" gutter={[16, 16]}>
+            <Col xs={24} md={6}>
+              <Row gutter={8} className="bg-white rounded-lg h-full py-6 ">
+                <Col md={8} className="mx-auto my-auto">
+                  <div className="flex     ">
+                    <div className="  bg-violet-300 mx-auto my-auto rounded-full ">
+                      <div className="px-3 py-3">
+                        <StarSmallIcon />
+                      </div>
+                    </div>
+                  </div>
+                </Col>
+                <Col md={16} className="mx-auto my-auto">
+                  <div className="flex flex-col my-auto">
+                    <div className="text-sm md:text-base font-medium">
+                      Total Rating
+                    </div>
+                    <div className="text-lg font-medium">0.2</div>
+                  </div>
+                </Col>
+              </Row>
+            </Col>
+            <Col xs={24} md={6}>
+              <Row gutter={8} className="bg-white rounded-lg h-full py-6 ">
+                <Col md={8} className="mx-auto my-auto">
+                  <div className="flex     ">
+                    <div className="  bg-violet-300 mx-auto my-auto rounded-full ">
+                      <div className="px-3 py-3">
+                        <CalanderIcon />
+                      </div>
+                    </div>
+                  </div>
+                </Col>
+                <Col md={16} className="mx-auto my-auto">
+                  <div className="flex flex-col my-auto">
+                    <div className="text-sm md:text-base font-medium">
+                      Next Due Date
+                    </div>
+                    <div className="text-lg font-medium">13/7/2022</div>
+                  </div>
+                </Col>
+              </Row>
+            </Col>
+            <Col xs={24} md={6}>
+              <Row gutter={8} className="bg-white rounded-lg h-full py-6">
+                <Col md={8} className="mx-auto my-auto">
+                  <div className="flex     ">
+                    <div className="  bg-violet-300 mx-auto my-auto rounded-full ">
+                      <div className="px-3 py-3">
+                        <UserIcon />
+                      </div>
+                    </div>
+                  </div>
+                </Col>
+                <Col md={16} className="mx-auto my-auto">
+                  <div className="flex flex-col my-auto">
+                    <div className="text-sm md:text-base font-medium">
+                      Assign to people
+                    </div>
+                    <div className="text-lg font-medium">20</div>
+                  </div>
+                </Col>
+              </Row>
+            </Col>
+          </Row>
           {/* <Table
             className="review-question-table"
             columns={columns}
@@ -114,7 +214,7 @@ function ReviewCreatedComponent({ user, reviewData }) {
 
           <Row gutter={[16, 16]}>
             <Col xs={24} md={24}>
-              <div className="overflow-x-auto">
+              <div className="overflow-x-auto mt-4">
                 <Collapse
                   accordion
                   defaultActiveKey={["1"]}
@@ -149,7 +249,7 @@ function ReviewCreatedComponent({ user, reviewData }) {
                               rowClassName={(_, index) =>
                                 index % 2 === 0 ? "" : "background-color-voilet"
                               }
-                              bordered
+                              bordered={true}
                             />
                           </Panel>
                         </>
