d504ce705729aeee14c2d00678e3ab8f206ccb45
ankit <ankit@zasyasolutions.com>
2022-07-26 17:40:10 +0530

    changes in answer component

--- component/Review/ReceivedReviewComponent.jsx
+++ component/Review/ReceivedReviewComponent.jsx
@@ -9,6 +9,7 @@ import {
   TextAreaComponent,
   SliderComponent,
   InputFormComponent,
+  FormSlideComponent,
 } from "./formhelper/FormComponent";
 
 function ReceivedReviewComponent({ user, reviewId }) {
@@ -17,6 +18,7 @@ function ReceivedReviewComponent({ user, reviewId }) {
   const [loading, setLoading] = useState(false);
   const [formValues, setFormValues] = useState([]);
   const [questions, setQuestions] = useState([]);
+  const [nextSlide, setNextSlide] = useState(0);
 
   const handleAnswerChange = (quesId, value) => {
     setFormValues((prev) =>
@@ -121,21 +123,46 @@ function ReceivedReviewComponent({ user, reviewId }) {
     <div className="px-3 md:px-8 h-auto mt-5">
       <div className="container mx-auto max-w-full">
         <div className="grid grid-cols-1 px-4 mb-16">
-          <div className="w-full bg-white rounded-xl overflow-hdden shadow-md p-4 ">
-            <div className="px-4 pb-4">
-              <div className="overflow-x-auto">
-                {loading ? (
-                  <Skeleton
-                    title={false}
-                    active={true}
-                    width={[200]}
-                    className="mt-4"
-                    rows={3}
-                  />
-                ) : (
-                  <>
-                    <InputComponent />
-                    <div className="w-full flex  flex-col items-start px-4 pt-4 pb-5 bg-gray-200 rounded">
+          {/* <div className="w-full bg-white rounded-xl overflow-hdden shadow-md p-4 "> */}
+          <div className="px-4 pb-4">
+            <div className="overflow-x-auto">
+              {loading ? (
+                <Skeleton
+                  title={false}
+                  active={true}
+                  width={[200]}
+                  className="mt-4"
+                  rows={3}
+                />
+              ) : (
+                <>
+                  <div className=" answer-bg h-full w-full px-4">
+                    {questions.length > 0 &&
+                      questions
+                        ?.filter((_, index) => index === nextSlide)
+                        ?.map((question, idx) => (
+                          <>
+                            <FormSlideComponent
+                              {...question}
+                              idx={idx}
+                              open={false}
+                              nextSlide={nextSlide}
+                              handleAnswerChange={handleAnswerChange}
+                              setNextSlide={setNextSlide}
+                              length={questions.length}
+                              handleSubmit={handleSubmit}
+                            />
+                          </>
+                        ))}
+
+                    {/* <button
+                      className="toggle-btn-bg rounded-md text-lg text-white px-14 py-2 "
+                      onClick={() => setNextSlide(nextSlide + 1)}
+                    >
+                      Next
+                    </button> */}
+
+                    {/* <div className="w-full flex  flex-col items-start px-4 pt-4 pb-5 bg-gray-200 rounded">
                       <div>
                         <h3 className="text-2xl font-medium primary-color-blue mb-2">
                           {reviewData?.review?.form?.form_title}
@@ -156,9 +183,9 @@ function ReceivedReviewComponent({ user, reviewId }) {
                             handleAnswerChange={handleAnswerChange}
                           />
                         </>
-                      ))}
+                      ))} */}
 
-                    <div className="flex justify-end mt-4">
+                    {/* <div className="flex justify-end mt-4">
                       <button
                         key="add"
                         className="profile-submit-button py-2 cursor-pointer primary-bg-btn text-white text-base  text-center rounded-md h-full w-32 mr-2"
@@ -167,6 +194,7 @@ function ReceivedReviewComponent({ user, reviewId }) {
                       >
                         Cancel
                       </button>
+
                       <button
                         key="add"
                         className="profile-submit-button py-2 cursor-pointer primary-bg-btn text-white text-base  text-center rounded-md h-full w-32"
@@ -175,12 +203,13 @@ function ReceivedReviewComponent({ user, reviewId }) {
                       >
                         Submit
                       </button>
-                    </div>
-                  </>
-                )}
-              </div>
+                    </div> */}
+                  </div>
+                </>
+              )}
             </div>
           </div>
+          {/* </div> */}
         </div>
       </div>
     </div>
--- component/Review/formhelper/FormComponent.jsx
+++ component/Review/formhelper/FormComponent.jsx
@@ -1,6 +1,156 @@
 import React, { createRef, useState } from "react";
-import { Button, Carousel, Input, Radio, Slider } from "antd";
+import { Button, Carousel, Col, Input, Radio, Row, Slider, Space } from "antd";
 import TextArea from "antd/lib/input/TextArea";
+
+export function FormSlideComponent({
+  type,
+  idx,
+  id,
+  questionText,
+  options,
+  handleAnswerChange,
+  lowerLabel,
+  higherLabel,
+  error = "",
+  nextSlide,
+  setNextSlide,
+  length,
+  handleSubmit,
+}) {
+  const [sliderInputValue, setSliderInputValue] = useState(0);
+
+  return (
+    <>
+      <div className="">
+        <div className="h-full w-full">
+          <Row>
+            <Col md={24} xs={24}>
+              <div className="text-center answer-preview">
+                <div className="py-24 flex flex-col items-center justify-center text-center">
+                  <p className="text-lg font-bold text-red-400 my-5">
+                    {`Question ${nextSlide + 1}`}
+                  </p>
+                  <p className="text-2xl font-bold primary-color-blue my-5">
+                    {questionText}
+                  </p>
+
+                  {options?.length > 0 && type === "checkbox" && (
+                    <Row>
+                      <Col>
+                        <Radio.Group
+                          className="radio-button"
+                          onChange={(e) =>
+                            handleAnswerChange(id, e.target.value)
+                          }
+                        >
+                          {/* <Space direction="vertical"> */}
+                          {options?.map((op, j) => (
+                            <>
+                              <Radio value={op.optionText}>
+                                {op.optionText}
+                              </Radio>
+                            </>
+                          ))}
+                          {/* </Space> */}
+                        </Radio.Group>
+                      </Col>
+                    </Row>
+                  )}
+
+                  {type == "input" ? (
+                    <div className="my-5 md:w-96 ">
+                      <Input
+                        fullWidth={true}
+                        placeholder={type == "input" ? "Short Text" : ""}
+                        rows={1}
+                        onChange={(e) => handleAnswerChange(id, e.target.value)}
+                      />
+                    </div>
+                  ) : null}
+
+                  {type === "textarea" ? (
+                    <div className="my-5 md:w-96 ">
+                      <TextArea
+                        fullWidth={true}
+                        placeholder={type == "textarea" ? "Long Text" : ""}
+                        rows={1}
+                        onChange={(e) => handleAnswerChange(id, e.target.value)}
+                      />
+                    </div>
+                  ) : null}
+
+                  {type === "scale" && options?.length > 1 && (
+                    <div className=" text-left md:w-96">
+                      <div className="flex w-full justify-center items-center">
+                        <p>{options[0]?.optionText}</p>
+                        <p className="w-full text-center mx-2 md:ml-4">
+                          <Slider
+                            className="rating-slider"
+                            min={Number(options[0]?.lowerLabel)}
+                            max={Number(options[0]?.higherLabel)}
+                            step={1}
+                            onChange={(e) => {
+                              handleAnswerChange(id, e.toString());
+                              setSliderInputValue(e);
+                            }}
+                            // value={typeof inputValue === "number" ? inputValue : 0}
+                          />
+                        </p>
+
+                        <p className="rounded-full bg-violet-400 text-white text-center mr-2 md:mr-3  py-1 md:py-2 px-1 md:px-3">
+                          {sliderInputValue}
+                        </p>
+
+                        <p>{options[1]?.optionText}</p>
+                      </div>
+                      {error ? (
+                        <p className="text-red-600 text-sm my-2">{error}</p>
+                      ) : null}
+                    </div>
+                  )}
+                  <div className="my-5">
+                    {nextSlide > 0 ? (
+                      <button
+                        className="toggle-btn-bg rounded-md text-lg text-white px-14 py-2 mx-2 "
+                        onClick={() => setNextSlide(nextSlide - 1)}
+                      >
+                        Pre
+                      </button>
+                    ) : (
+                      ""
+                    )}
+
+                    {length - 1 === nextSlide ? (
+                      <button
+                        className="toggle-btn-bg rounded-md text-lg text-white px-10 py-2 mx-2 my-2 "
+                        onClick={() => handleSubmit()}
+                      >
+                        Submit
+                      </button>
+                    ) : (
+                      <button
+                        className="toggle-btn-bg rounded-md text-lg text-white px-14 py-2 mx-2 my-2 "
+                        onClick={() => setNextSlide(nextSlide + 1)}
+                      >
+                        Next
+                      </button>
+                    )}
+                    {/* <button
+                      className="toggle-btn-bg rounded-md text-lg text-white px-14 py-2 mx-2 "
+                      onClick={() => setNextSlide(nextSlide + 1)}
+                    >
+                      Next
+                    </button> */}
+                  </div>
+                </div>
+              </div>
+            </Col>
+          </Row>
+        </div>
+      </div>
+    </>
+  );
+}
 
 export function InputComponent() {
   return (
@@ -133,128 +283,3 @@ export function SliderComponent() {
     </>
   );
 }
-
-// export class InputFormComponent extends React.Component {
-//   constructor(props) {
-//     super(props);
-//     this.carousel = createRef();
-//   }
-
-//   // onChange = (1, 2, 3) => {
-//   //   console.log(a, b, c);
-//   // };
-
-//   handleNext = () => this.carousel.current.next();
-
-//   handlePrev = () => this.carousel.current.prev();
-
-//   render() {
-//     return (
-//       <div className="answer-bg h-full w-full ">
-//         <div className="text-center py-8">
-//           <Carousel afterChange={this.onChange} ref={this.carousel}>
-//             <div className="text-center">
-//               <div className="py-24 flex flex-col items-center justify-center text-center">
-//                 <p className="text-xl font-bold my-5 text-red-400">
-//                   Question 1
-//                 </p>
-//                 <p className="text-2xl font-bold primary-color-blue my-5">
-//                   What do you feel about your current work environment?
-//                 </p>
-//                 <div className="my-5 w-96">
-//                   <Input size="large" />
-//                 </div>
-//                 {/* <div className="my-5">
-//                     <button
-//                       className="toggle-btn-bg rounded-md text-lg text-white px-14 py-2 "
-//                       onClick={next()}
-//                     >
-//                       Next
-//                     </button>
-//                   </div> */}
-//               </div>
-//             </div>
-
-//             <div className="text-center">
-//               <div className="py-24 flex flex-col items-center justify-center">
-//                 <p className="text-xl font-bold my-5 text-red-400">
-//                   Question 2
-//                 </p>
-//                 <p className="text-2xl font-bold primary-color-blue my-5">
-//                   What do you feel about your current work environment?
-//                 </p>
-//                 <div className="my-5 w-96 add-template-wrapper">
-//                   <Radio.Group>
-//                     <Radio
-//                       value={1}
-//                       className="text-lg primary-color-blue my-2"
-//                     >
-//                       Feedback
-//                     </Radio>
-//                     <br />
-//                     <Radio
-//                       value={2}
-//                       className="text-lg primary-color-blue my-2"
-//                     >
-//                       Create
-//                     </Radio>
-//                     <br />
-//                     <Radio
-//                       value={3}
-//                       className="text-lg primary-color-blue my-2"
-//                     >
-//                       Template
-//                     </Radio>
-//                     <br />
-//                     <Radio
-//                       value={4}
-//                       className="text-lg primary-color-blue my-2"
-//                     >
-//                       Option
-//                     </Radio>
-//                   </Radio.Group>
-//                 </div>
-//               </div>
-//             </div>
-
-//             <div className="text-center">
-//               <div className="py-24 flex flex-col items-center justify-center">
-//                 <p className="text-xl font-bold my-5 text-red-400">
-//                   Question 3
-//                 </p>
-//                 <p className="text-2xl font-bold primary-color-blue my-5">
-//                   Textarea to select feedback or create new template
-//                 </p>
-//                 <div className=" text-left w-96">
-//                   <TextArea rows={4} />
-//                 </div>
-//               </div>
-//             </div>
-
-//             <div className="text-center">
-//               {" "}
-//               <div className="py-24 flex flex-col items-center justify-center">
-//                 <p className="text-xl font-bold my-5 text-red-400">
-//                   Question 4
-//                 </p>
-//                 <p className="text-2xl font-bold primary-color-blue my-5">
-//                   Rating
-//                 </p>
-//                 <div className=" text-left w-96">
-//                   <Slider className="rating-slider-a" />
-//                 </div>
-//               </div>
-//             </div>
-//           </Carousel>
-
-//           <button
-//             className="toggle-btn-bg rounded-md text-lg text-white px-14 py-2"
-//             onClick={this.handleNext}
-//           >
-//             Next
-//           </button>
-//         </div>
-//       </div>
-//     );
-//   }
-// }
