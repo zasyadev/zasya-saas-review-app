1662a081d4a6b38f06d6fab356dccaca997d52a5
zasyadev <hello@zasyasolutions.com>
2022-11-25 12:14:31 +0530

    question preview route

--- pages/api/review/received/[review_id].js
+++ pages/api/review/received/[review_id].js
@@ -2,43 +2,70 @@ import { RequestHandler } from "../../../../lib/RequestHandler";
 
 async function handle(req, res, prisma) {
   const { review_id } = req.query;
-  const { userId } = req.body;
 
-  if (!userId && !review_id) {
-    return res.status(401).json({ status: 401, message: "No Record found" });
-  }
+  if (req.method === "POST") {
+    const { userId } = req.body;
+    if (!userId && !review_id) {
+      return res.status(401).json({ status: 401, message: "No Record found" });
+    }
 
-  const data = await prisma.reviewAssignee.findFirst({
-    where: {
-      AND: [
-        { id: review_id },
-        {
-          assigned_to_id: userId,
-        },
-      ],
-    },
-    include: {
-      review: {
-        include: {
-          created: true,
-          form: {
-            include: {
-              questions: {
-                include: { options: true },
+    const data = await prisma.reviewAssignee.findFirst({
+      where: {
+        AND: [
+          { id: review_id },
+          {
+            assigned_to_id: userId,
+          },
+        ],
+      },
+      include: {
+        review: {
+          include: {
+            created: true,
+            form: {
+              include: {
+                questions: {
+                  include: { options: true },
+                },
               },
             },
           },
         },
       },
-    },
-  });
+    });
 
-  return res.status(200).json({
-    status: 200,
-    data: data,
-    message: "Review Details Retrieved",
-  });
+    return res.status(200).json({
+      status: 200,
+      data: data,
+      message: "Review Details Retrieved",
+    });
+  }
+
+  if (req.method === "GET") {
+    const data = await prisma.review.findUnique({
+      where: {
+        id: review_id,
+      },
+      include: {
+        created: true,
+        form: {
+          include: {
+            questions: {
+              include: { options: true },
+            },
+          },
+        },
+      },
+    });
+
+    return res.status(200).json({
+      status: 200,
+      data: data,
+      message: "Review Details Retrieved",
+    });
+  }
 }
-const functionHandle = (req, res) => RequestHandler(req, res, handle, ["POST"]);
+const functionHandle = (req, res) =>
+  RequestHandler(req, res, handle, ["POST", "GET"]);
 
 export default functionHandle;
