292821b087da12393357375b6015b11b0ab62d68
zasyadev <hello@zasyasolutions.com>
2022-11-30 17:25:19 +0530

    survey answer changes

--- pages/api/survey/getSurveyByUrl/index.js
+++ pages/api/survey/getSurveyByUrl/index.js
@@ -3,23 +3,39 @@ import { RequestHandler } from "../../../../lib/RequestHandler";
 async function handle(req, res, prisma) {
   if (req.method === "POST") {
     const { urlId } = req.body;
-    const data = await prisma.survey.findFirst({
-      where: { url_id: urlId },
-      include: {
-        SurveyQuestions: {
-          include: {
-            SurveyQuestionOption: true,
-          },
-        },
-      },
+    const data = await prisma.surveyChannel.findFirst({
+      where: { url: urlId },
     });
 
     if (data) {
-      return res.status(200).json({
-        status: 200,
-        data: data,
-        message: "Survey Details Retrieved",
+      const surveyData = await prisma.survey.findFirst({
+        where: { id: data.survey_id },
+        include: {
+          SurveyQuestions: {
+            include: {
+              SurveyQuestionOption: true,
+            },
+          },
+        },
       });
+      if (!surveyData) {
+        return res
+          .status(404)
+          .json({ status: 404, message: "No Record Found" });
+      }
+      if (surveyData && surveyData.status) {
+        return res.status(200).json({
+          status: 200,
+          data: data,
+          message: "Survey Details Retrieved",
+        });
+      } else {
+        return res.status(401).json({
+          status: 401,
+          data: data,
+          message: "Survey is Inactive",
+        });
+      }
     }
 
     return res.status(404).json({ status: 404, message: "No Record Found" });
