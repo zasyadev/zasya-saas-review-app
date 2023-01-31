c32e602776dabf85f78f5fcafb3011e3881b7bf6
kshtjsharma68 <kshtjsharma68@gmail.com>
2023-01-31 17:16:15 +0530

    clear all notification api

--- component/layout/components/NotificationMenus.jsx
+++ component/layout/components/NotificationMenus.jsx
@@ -1,22 +1,33 @@
-import Link from "next/link";
 import React from "react";
+import Link from "next/link";
+import moment from "moment";
 
 const NotificationMenus = ({
   allNotification,
   unSeenNotificationCount,
   notificationViewed,
+  deleteNotifications,
 }) => {
   return (
     <div className="notification-wrapper border border-gray-100 bg-white shadow-xl rounded-md">
       <div className="flex items-center justify-between border-b border-gray-300 p-2 md:p-3 gap-6">
         <p className="text-sm lg:text-base font-bold mb-0">Notifications</p>
-        {unSeenNotificationCount > 0 && (
+        {unSeenNotificationCount > 0 ? (
           <button
             className="font-semibold text-xs text-primary rounded-full"
             onClick={() => notificationViewed("ALL")}
           >
             Mark all as read
           </button>
+        ) : (
+          allNotification.length > 0 && (
+            <button
+              className="font-semibold text-xs text-gray-600 rounded-full"
+              onClick={() => deleteNotifications()}
+            >
+              Clear all
+            </button>
+          )
         )}
       </div>
       <div className="notification-inner no-scrollbar divide-y py-1">
@@ -32,13 +43,20 @@ const NotificationMenus = ({
               onClick={() => notificationViewed(item.id)}
             >
               <Link href={item.data.link} passHref>
-                <p
-                  className={`${
-                    item.read_at ? "text-gray-500" : ""
-                  } hover:bg-gray-50 text-sm p-2 font-medium md:p-3 mb-0 cursor-pointer`}
-                >
-                  {item.data.message}
-                </p>
+                <div className="p-2 md:p-3 ">
+                  <p
+                    className={`${
+                      item.read_at ? "text-gray-500" : ""
+                    } hover:bg-gray-50 text-sm  font-medium  mb-0 cursor-pointer`}
+                  >
+                    {item.data.message}
+                  </p>
+                  {item?.created_date && (
+                    <p className="mb-0  text-gray-400  text-xs leading-4">
+                      {moment(item.created_date).fromNow()}
+                    </p>
+                  )}
+                </div>
               </Link>
             </div>
           ))
