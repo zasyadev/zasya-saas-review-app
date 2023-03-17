041aecd446aa5e3aa00fc4dabd2bda7563a26b38
zasyadev <hello@zasyasolutions.com>
2023-03-17 17:19:17 +0530

    removed unwanted lines from dashboard chart

--- component/common/BarChart.jsx
+++ component/common/BarChart.jsx
@@ -49,89 +49,6 @@ const defaultOptions = {
   },
 };
 
-// const defaultOptions = {
-//   chart: {
-//     toolbar: {
-//       show: false,
-//     },
-//     type: "bar",
-//     height: 360,
-//   },
-//   plotOptions: {
-//     bar: {
-//       borderRadius: 14,
-//       columnWidth: "40%",
-//       dataLabels: {
-//         position: "top",
-//       },
-//     },
-//   },
-//   responsive: [
-//     {
-//       breakpoint: 500,
-//       options: {
-//         plotOptions: {
-//           bar: {
-//             borderRadius: 1,
-//             columnWidth: "90%",
-//           },
-//         },
-//       },
-//     },
-//   ],
-//   dataLabels: {
-//     enabled: true,
-//     formatter: function (val) {
-//       return val;
-//     },
-//     offsetY: 10,
-//     style: {
-//       fontSize: "12px",
-//       colors: ["#fff"],
-//     },
-//   },
-//   xaxis: {
-//     categories: [
-//       "Jan",
-//       "Feb",
-//       "Mar",
-//       "Apr",
-//       "May",
-//       "Jun",
-//       "Jul",
-//       "Aug",
-//       "Sep",
-//       "Oct",
-//       "Nov",
-//       "Dec",
-//     ],
-//     position: "bottom",
-//     axisBorder: {
-//       show: false,
-//     },
-//     axisTicks: {
-//       show: false,
-//     },
-//   },
-
-//   colors: ["#0F123F"],
-//   grid: {
-//     yaxis: {
-//       lines: {
-//         show: false,
-//       },
-//     },
-//     lines: {
-//       show: false,
-//     },
-//   },
-//   tooltip: {
-//     custom: function ({ series, seriesIndex, dataPointIndex, w }) {
-//       return `<div className="tooltip_chart"> <span className="px-2"> ${w.globals.labels[dataPointIndex]} : ${series[seriesIndex][dataPointIndex]} </span> </div>`;
-//     },
-//   },
-// };
-
 const BarChart = ({ user }) => {
   const [chartData, setChartData] = useState(defaultChartSeries);
   const [loading, setLoading] = useState(true);
@@ -171,7 +88,6 @@ const BarChart = ({ user }) => {
       .catch((err) => {
         setLoading(false);
         setChartData(defaultChartSeries);
-        console.error(err.response.data?.message);
       });
   }
   useEffect(() => {
@@ -180,14 +96,6 @@ const BarChart = ({ user }) => {
 
   return (
     <div>
-      {/* <p className="chart-title text-primary flex items-center justify-between">
-        Feedback
-        <span className="leading-0 text-base text-gray-900 ml-2">
-          {CustomPopover(
-            "Count of Feedback given by the organization monthly."
-          )}
-        </span>
-      </p> */}
       {loading ? (
         <>
           <Skeleton active className="mt-4 mb-2" />
