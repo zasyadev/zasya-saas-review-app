c9142d171360ac88c422ecceb4cd6652bb6956d6
zasyadev <hello@zasyasolutions.com>
2023-04-13 14:40:39 +0530

    activities api refector and exception handling

--- pages/api/activity/[user_id].js
+++ pages/api/activity/[user_id].js
@@ -1,3 +1,4 @@
+import { BadRequestException } from "../../../lib/BadRequestExcpetion";
 import { RequestHandler } from "../../../lib/RequestHandler";
 
 async function handle(req, res, prisma) {
@@ -7,6 +8,8 @@ async function handle(req, res, prisma) {
     where: { id: user_id },
   });
 
+  if (!userData) throw BadRequestException("User not found");
+
   const activityData = await prisma.userActivity.findMany({
     orderBy: {
       id: "desc",
@@ -14,15 +17,14 @@ async function handle(req, res, prisma) {
     where: {
       AND: [
         { user_id: user_id },
-        {
-          organization_id: userData.organization_id,
-        },
+        { organization_id: userData.organization_id },
       ],
     },
   });
 
+  if (!activityData) throw BadRequestException("Record not found");
+
   return res.status(200).json({
-    status: 200,
     data: activityData,
     message: "All Data Retrieved",
   });
